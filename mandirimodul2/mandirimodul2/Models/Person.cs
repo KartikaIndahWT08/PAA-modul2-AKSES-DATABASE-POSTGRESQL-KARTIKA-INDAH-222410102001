37330fe4bcda614f51927b4f939369bf0012ab4a
KartikaIndahWT08 <117880126+KartikaIndahWT08@users.noreply.github.com>
2024-03-22 13:15:54 +0700

    Add files via upload

---
+++ mandirimodul2/mandirimodul2/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace mandirimodul2.Models
+{
+    public class Murid
+    {
+        public int id_person { get; set; }
+        public string nama { get; set; }
+        public string alamat { get; set; }
+        public string email { get; set; }
+    }
+
+}
